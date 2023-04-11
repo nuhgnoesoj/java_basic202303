83357ac743657434fcf6d97e9037e9136fce2bc6
nuhgnoesoj <126743224+nuhgnoesoj@users.noreply.github.com>
2023-04-11 09:00:36 +0900

    파일 입출력

--- src/day10/collection/song/Main.java
+++ src/day10/collection/song/Main.java
@@ -2,6 +2,10 @@ package day10.collection.song;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 세이브파일 자동로드
+        ArtistRepository.loadFile();
+
         ArtistView.start();
     }
 }
